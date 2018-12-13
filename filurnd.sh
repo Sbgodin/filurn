4075242dcaf5086e5618993d41b5f645b9aaf804
Christophe HENRY <christophe.henry@sbgodin.fr>
2018-12-13 11:35:04 +0100

    Error on file name fixed

--- filurnd.sh
+++ filurnd.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="/usr/local/bin/filurn"
 while : 
 do 
 	if [ -f $MOUNT/$MARKER ] ;then
-		inotifywait $MOUNT/$MARKER && $SCRIPT_DIR/filurn.sh
+		inotifywait $MOUNT/$MARKER && $SCRIPT_DIR/filurnd.sh
 	else
 		echo "mounting... $MOUNT/$MARKER not found."
 		$SCRIPT_DIR/filurn-mount.sh		
